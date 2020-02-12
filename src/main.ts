4cdf6d06a9f465288679aa9d908b8a0c2472c588
jonathborg <jonathborg@gmail.com>
2020-02-12 07:30:48 -0300

    validators

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
 import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { AppModule } from './modules/AppModule';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+    app.useGlobalPipes(new ValidationPipe());
     await app.listen(process.env.PORT || 3001, () => {
         Logger.log(
             `🚀\tServer is running on port ${process.env.PORT ||
